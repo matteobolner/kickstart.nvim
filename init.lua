5c6ae8b59b559c2e1dab0f5ffc3b52ae45eacaf4
matteobolner <matteo.bolner@studio.unibo.it>
2025-01-07 16:42:43 +0100

    Added my fork of cmp-path

--- init.lua
+++ init.lua
@@ -764,7 +764,7 @@ require('lazy').setup({
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+      'matteobolner/cmp-path',
     },
     config = function()
       -- See `:help cmp`
